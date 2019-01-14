b7b0fde5265cc96c6b5f88395d004874b5f74835
Daichi Ito <math144377@gmail.com>
2019-01-14 12:51:37 +0900

    toggle ray

--- SpoutSRPlayer/Assets/Scripts/RayManager.cs
+++ SpoutSRPlayer/Assets/Scripts/RayManager.cs
@@ -7,6 +7,7 @@ public class RayManager : MonoBehaviour
     private Camera sceneCamera;
     private CalibrationDemo calibrationDemo;
     private LineRenderer heading;
+    private List<MeshRenderer> gazeRenderers;
 
     private Vector3 standardViewportPoint = new Vector3 (0.5f, 0.5f, 10); // default marker position
 
@@ -34,6 +35,27 @@ public class RayManager : MonoBehaviour
 
         // TODO: Findを使わない実装
         getControllerState = GameObject.Find ("[CameraRig]").GetComponent<GetControllerState> ();
+
+        // TODO: tagで綺麗に取得
+        GameObject mainCamera = GameObject.Find ("MainCamera");
+        GameObject leftEye = mainCamera.transform.Find ("LeftEye_2D").gameObject;
+        GameObject leftEyePoint = leftEye.transform.Find ("MarkerEye").gameObject;
+        GameObject rightEye = mainCamera.transform.Find ("RightEye_2D").gameObject;
+        GameObject rightEyePoint = rightEye.transform.Find ("MarkerEye").gameObject;
+        GameObject gaze2D = mainCamera.transform.Find ("Gaze_2D").gameObject;
+        GameObject gaze2DPoint = gaze2D.transform.Find ("MarkerEye").gameObject;
+        GameObject gaze3D = mainCamera.transform.Find ("Gaze_3D").gameObject;
+        GameObject gaze3DPoint = gaze3D.transform.Find ("MarkerEye").gameObject;
+
+        gazeRenderers = new List<MeshRenderer> ();
+        gazeRenderers.Add (leftEye.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (leftEyePoint.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (rightEye.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (rightEyePoint.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (gaze2D.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (gaze2DPoint.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (gaze3D.GetComponent<MeshRenderer> ());
+        gazeRenderers.Add (gaze3DPoint.GetComponent<MeshRenderer> ());
     }
 
     void OnEnable ()
@@ -61,20 +83,27 @@ public class RayManager : MonoBehaviour
         // if (Input.GetKeyUp(KeyCode.M))
         //     monoColorMode = !monoColorMode;
 
-        bool isLeftGripped = getControllerState.leftControllerGripped;
+        // bool isLeftGripped = getControllerState.leftControllerGripped;
         bool isRightGripped = getControllerState.rightControllerGripped;
 
         // if (Input.GetKeyUp (KeyCode.G))
-        if (Input.GetKeyUp (KeyCode.G) || isLeftGripped)
-        {
-            calibrationDemo.enabled = !calibrationDemo.enabled;
-            heading.enabled = calibrationDemo.enabled ? true : false;
-        }
+        // if (Input.GetKeyUp (KeyCode.G) || isLeftGripped)
+        // {
+        //     calibrationDemo.enabled = !calibrationDemo.enabled;
+        //     heading.enabled = calibrationDemo.enabled ? true : false;
+        // }
 
         // if (Input.GetKeyUp (KeyCode.L))
         if (Input.GetKeyUp (KeyCode.L) || isRightGripped)
             if (calibrationDemo.enabled)
+            {
                 heading.enabled = !heading.enabled;
+                for (int i = 0; i < gazeRenderers.Count; i++)
+                {
+                    bool isRendered = gazeRenderers[i].enabled;
+                    gazeRenderers[i].enabled = !isRendered;
+                }
+            }
 
         if (heading.enabled)
         {
