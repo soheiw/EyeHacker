813b60ed88e100e34ea2cbfeda1e96fea4b73126
radi-bow <math144377@gmail.com>
2019-02-10 22:04:16 +0900

    Add spoutBackgroundSubtractor.py

--- Python/showGraph.py
+++ Python/showGraph.py
@@ -4,7 +4,7 @@ import math
 
 f_sqrt = lambda x: math.sqrt(x)
 
-df = pd.read_csv('wholeRisk101629.csv', names=['num1', 'risk', 'rotation'])
+df = pd.read_csv('wholeRisk102155.csv', names=['num1', 'risk', 'rotation'])
 #plt.plot(df['num1'], df['risk'].apply(f_sqrt))
 plt.plot(df['num1'], df['risk'])
 plt.plot(df['num1'], df['rotation'])
