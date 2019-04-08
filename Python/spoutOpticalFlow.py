9f7b99bb9b11298ae15f06c868b59a68ac19d88c
radi-bow <math144377@gmail.com>
2019-04-08 14:18:20 +0900

    recreate environments for new PC

--- Python/spoutOpticalFlow.py
+++ Python/spoutOpticalFlow.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Add relative directory ../Library to import path, so we can import the SpoutSDK.pyd library. Feel free to remove these if you put the SpoutSDK.pyd file in the same directory as the python scripts.
 import cv2
 import numpy as np
