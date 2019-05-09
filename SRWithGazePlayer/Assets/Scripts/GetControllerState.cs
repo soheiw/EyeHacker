ac0359583dfedc9e120d4a5a9364c35144cf8a08
radi-bow <math144377@gmail.com>
2019-05-09 18:00:51 +0900

    renew text

--- SRWithGazePlayer/Assets/Scripts/GetControllerState.cs
+++ SRWithGazePlayer/Assets/Scripts/GetControllerState.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class GetControllerState : MonoBehaviour
 {
     [SerializeField] private GameObject leftController;
-    [SerializeField] private GameObject rightController;
+    // [SerializeField] private GameObject rightController;
 
     public bool leftControllerTouchpadPressUp;
-    public bool rightControllerTouchpadPressUp;
+    // public bool rightControllerTouchpadPressUp;
 
     public bool leftControllerTriggerPressUp;
-    public bool rightControllerTriggerPressUp;
+    // public bool rightControllerTriggerPressUp;
 
     public bool leftControllerGripped;
-    public bool rightControllerGripped;
+    // public bool rightControllerGripped;
 
     // Use this for initialization
     void Start ()
@@ -26,19 +26,19 @@ public class GetControllerState : MonoBehaviour
     void Update ()
     {
         var leftStatus = SteamVR_Controller.Input ((int) leftController.GetComponent<SteamVR_TrackedObject> ().index);
-        var rightStatus = SteamVR_Controller.Input ((int) rightController.GetComponent<SteamVR_TrackedObject> ().index);
+        // var rightStatus = SteamVR_Controller.Input ((int) rightController.GetComponent<SteamVR_TrackedObject> ().index);
 
         var touchpad = SteamVR_Controller.ButtonMask.Touchpad;
         var trigger = SteamVR_Controller.ButtonMask.Trigger;
         var grip = SteamVR_Controller.ButtonMask.Grip;
 
         leftControllerTouchpadPressUp = leftStatus.GetPressUp (touchpad);
-        rightControllerTouchpadPressUp = rightStatus.GetPressUp (touchpad);
+        // rightControllerTouchpadPressUp = rightStatus.GetPressUp (touchpad);
 
         leftControllerTriggerPressUp = leftStatus.GetPressUp (trigger);
-        rightControllerTriggerPressUp = rightStatus.GetPressUp (trigger);
+        // rightControllerTriggerPressUp = rightStatus.GetPressUp (trigger);
 
         leftControllerGripped = leftStatus.GetPressDown (grip);
-        rightControllerGripped = rightStatus.GetPressDown (grip);
+        // rightControllerGripped = rightStatus.GetPressDown (grip);
     }
 }
