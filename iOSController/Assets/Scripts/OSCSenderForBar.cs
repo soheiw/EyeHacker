c6eecc156bfd8d7410b5d59a8f8c1093c15c492e
Daichi Ito <math144377@gmail.com>
2019-05-29 10:30:57 +0900

    add uOSC

---
+++ iOSController/Assets/Scripts/OSCSenderForBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using uOSC;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSCSenderForBar : MonoBehaviour
+{
+    public uOscClient client;
+    public string address;
+
+    public void OnValueChanged (float v)
+    {
+        float baseline = 10 + v * 30;
+        Debug.Log ("Send" + address);
+        client.Send (address, baseline);
+    }
+}
---
+++ iOSController/Assets/Scripts/OSCSenderForToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using uOSC;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSCSenderForToggle : MonoBehaviour
+{
+    public uOscClient client;
+    public string address;
+
+    public void OnToggleChanged (Toggle toggle)
+    {
+        if (toggle.isOn)
+        {
+            Debug.Log ("Send" + address);
+            client.Send (address, 1);
+        }
+        else
+        {
+            Debug.Log ("Send" + address);
+            client.Send (address, 0);
+        }
+    }
+}
