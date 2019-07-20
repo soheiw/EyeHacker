09d1a506a82e31eb81dbbb1185a8edb566256cd8
radi-bow <math144377@gmail.com>
2019-07-21 00:21:13 +0900

    ignore gaze data if distance between frames is longer than thr

--- SRWithGazePlayer/Assets/Scripts/RayManager.cs
+++ SRWithGazePlayer/Assets/Scripts/RayManager.cs
@@ -20,6 +20,8 @@ public class RayManager : MonoBehaviour
     private Vector2 gazePointRight;
     private Vector2 gazePointCenter;
 
+    public PupilSettings setting;
+
     [SerializeField] private GetControllerState getControllerState;
 
     public GameObject gazePos;
@@ -166,14 +168,32 @@ public class RayManager : MonoBehaviour
         if (Physics.Raycast (ray, out hit))
         {
             gazePosition = hit.point;
-            gazeCoord = hit.textureCoord;
-            gazeCoord.x *= textureSize.x;
-            gazeCoord.y *= textureSize.y;
+
+            if (isFirstEyeTrack)
+            {
+                isFirstEyeTrack = false;
+                prevGazePosition = gazePosition;
+            }
+
+            if (Vector3.Distance (prevGazePosition, gazePosition) < gazeDistanceThr)
+            {
+                gazeCoord = hit.textureCoord;
+                gazeCoord.x *= textureSize.x;
+                gazeCoord.y *= textureSize.y;
+
+                prevGazePosition = gazePosition;
+            }
+            else
+            {
+                Debug.Log ("gaze movement: " + Vector3.Distance (prevGazePosition, gazePosition) + ", Threshold: " + setting.connection.confidenceThreshold);
+                gazePosition = prevGazePosition;
+            }
         }
         else
         {
             gazePosition = ray.origin + ray.direction * 50f;
             gazeCoord = new Vector2 (INF, INF);
+            Debug.Log ("raycast failed.");
         }
 
         gazePos.transform.position = gazePosition;
